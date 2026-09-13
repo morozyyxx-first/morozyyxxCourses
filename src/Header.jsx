4bdf9d3afc88ac75fbd3613b3be7478b66367492
morozyyxx <morozyyxx@gmail.com>
2026-09-14 00:31:35 +0200

    fix imports 2

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,16 +5,16 @@ function Header() {
     return (
         <header>
             <img src={python_image} alt="Python image"/>
-            <h3 id="main-name"><a href="../index.html">morozyyxx courses</a></h3>
+            <h3 id="main-name"><a href="index.html">morozyyxx courses</a></h3>
             <div id="header-buttons">
                 <button className="header-btn">
-                    <a href="../kindofcourses.html">Kind of courses</a>
+                    <a href="kindofcourses.html">Kind of courses</a>
                 </button>
                 <button className="header-btn">
                     <a href="https://viktormorozcv.vercel.app">About author</a>
                 </button>
                 <button className="header-btn">
-                    <a href="../forwho.html">For who?</a>
+                    <a href="forwho.html">For who?</a>
                 </button>
             </div>
             <button id="contact-btn">
